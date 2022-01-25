6dfdf14f78e14a6813e811f62c2abfff1d88985d
Virgnia <virginia@devanddel.com>
2022-01-25 18:03:14 +0100

    Currency pipe + default configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import localeES from '@angular/common/locales/es';
+import localeEsExtra from '@angular/common/locales/extra/es';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -10,11 +13,13 @@ import { AppComponent } from './app.component';
 import { ButtonCardComponent } from './home/home-page/components/button-card/button-card.component';
 import { ProductCardComponent } from './home/home-page/components/product-card/product-card.component';
 import { HomePageComponent } from './home/home-page/home-page.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { UserPageComponent } from './user/user-page/user-page.component';
 import { ProductPageComponent } from './product/product-page/product-page.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { OverShadowDirective } from './shared/directives/over-shadow.directive';
+import { UserPageComponent } from './user/user-page/user-page.component';
 
+
+registerLocaleData(localeES, 'es-ES', localeEsExtra);
 
 @NgModule({
   declarations: [
@@ -25,7 +30,7 @@ import { OverShadowDirective } from './shared/directives/over-shadow.directive';
     ProductCardComponent,
     ButtonCardComponent,
     ProductPageComponent,
-    OverShadowDirective
+    OverShadowDirective,
   ],
   imports: [
     BrowserModule,
@@ -36,7 +41,7 @@ import { OverShadowDirective } from './shared/directives/over-shadow.directive';
     MatIconModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-ES' }, { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
