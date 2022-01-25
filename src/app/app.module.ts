99a07711bb34cdc6a667be8a11fb28ca17a1110b
Virgnia <virginia@devanddel.com>
2022-01-25 10:47:29 +0100

    Set header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home/home-page/home-page.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { UserPageComponent } from './user/user-page/user-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    UserPageComponent
+    UserPageComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
