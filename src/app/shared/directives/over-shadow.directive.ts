00dacd6bee2667eea0c5636da3cb12065ec71b81
Virgnia <virginia@devanddel.com>
2022-01-25 16:03:43 +0100

    Custom attribute Directive

---
+++ src/app/shared/directives/over-shadow.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appOverShadow]',
+})
+export class OverShadowDirective {
+
+  @Input() appOverShadow = '';
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setShadow(this.appOverShadow || 'grey')
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+
+    this.setShadow(null);
+  }
+
+  constructor(private el: ElementRef) {}
+
+  private setShadow(color: string | null) {
+    debugger;
+    if(color) {
+      this.el.nativeElement.style.boxShadow = '0 2px 8px 0 ' + color
+    } else {
+      this.el.nativeElement.style.boxShadow = 'none';
+    }
+
+  }
+}
