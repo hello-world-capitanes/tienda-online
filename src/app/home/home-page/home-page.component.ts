8cb53871cf086e2c9c1203d8cdd08b3c1f2f5aa9
Virgnia <virginia@devanddel.com>
2022-02-04 10:12:23 +0100

    Don't forget unsubcribe observables

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { URL_SHOP } from 'src/app/core/url.constants';
 import { getId, Product, ProductCard } from 'src/app/models/producto.model';
 import { ProductsService } from 'src/app/services/products.service';
@@ -11,36 +12,47 @@ import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, OnDestroy {
+  listProducts: (Product | ProductCard)[] = [];
 
-  listProducts:  (Product | ProductCard) [] = [];
+  subscription!: Subscription;
 
-
-  constructor(private productsService: ProductsService, private shoppinCartService: ShoppingCartService, private router: Router) {}
+  constructor(
+    private productsService: ProductsService,
+    private shoppinCartService: ShoppingCartService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-   this.listProducts = this.productsService.getAllProducts()
+    this.listProducts = this.productsService.getAllProducts();
 
-   console.log(this.listProducts);
+    console.log(this.listProducts);
 
-   this.shoppinCartService.hasChanged$.subscribe( productUpdated => {
-      const index = this.listProducts.findIndex(
-        (product) =>
+    this.subscription = this.shoppinCartService.hasChanged$.subscribe(
+      (productUpdated) => {
+        const index = this.listProducts.findIndex(
+          (product) => productUpdated.product.id === getId(product)
+        );
 
-          productUpdated.product.id === getId(product)
-      );
-
-      this.listProducts[index] = productUpdated;
-   });
+        this.listProducts[index] = productUpdated;
+      }
+    );
   }
 
-  goToDetail(product: (Product | ProductCard)) {
-
-    let realProduct: Product = product.hasOwnProperty('product') ? (product as ProductCard).product :product as Product;
+  goToDetail(product: Product | ProductCard) {
+    let realProduct: Product = product.hasOwnProperty('product')
+      ? (product as ProductCard).product
+      : (product as Product);
     this.router.navigate([
       URL_SHOP.PRODUCT_DETAIL,
       realProduct.id,
       realProduct.name,
     ]);
+  }
+
+  ngOnDestroy(): void {
+    if (!!this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
